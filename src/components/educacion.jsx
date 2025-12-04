21f3df18ffce58cc147c0fb57b62247d1eb85d60
karengm27 <karengmg27@gmail.com>
2025-12-03 22:34:04 -0500

    feat: componente Educacion con formación complementaria

---
+++ src/components/educacion.jsx
@@ -0,0 +1,27 @@
+function Educacion() {
+  return (
+    <section>
+      <h3>Formación Académica</h3>
+
+      <ul>
+        <li>
+          <strong>Tecnólogo en Análisis y Desarrollo de Software</strong> – SENA (2025)
+        </li>
+        <li>
+          <strong>Curso de React</strong> – Udemy (2023)
+        </li>
+        <li>
+          <strong>JavaScript Moderno</strong> – Platzi (2022)
+        </li>
+        <li>
+          <strong>HTML y CSS</strong> – Google Activate (2021)
+        </li>
+        <li>
+          <strong>tecnico en desarrollo de software</strong> – Sena (2014)
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Educacion;
