00d686f492b3bb8006d1118f571a988163b37cf9
karengm27 <karengmg27@gmail.com>
2025-12-03 22:30:49 -0500

    feat: componente Perfil con descripción personalizada

---
+++ src/components/perfil.jsx
@@ -0,0 +1,21 @@
+function Perfil() {
+  return (
+    <section>
+      <h3>Perfil Profesional</h3>
+      <p>
+        Soy una persona apasionada por el desarrollo web principalmente con interfaces amigables con el usuario, dinamicas y faciles de usar, con enfoque en React,
+        buenas prácticas y diseño limpio. Me destaco por mi responsabilidad,
+        aprendizaje rápido y trabajo en equipo.
+      </p>
+
+      <h4>Habilidades</h4>
+      <ul>
+        <li>React – HTML – CSS – JavaScript</li>
+        <li>Trabajo en equipo</li>
+        <li>Comunicación asertiva</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Perfil;
