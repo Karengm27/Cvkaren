f72a89f2506011b3d56063c4644c16ab648bba8d
karengm27 <karengmg27@gmail.com>
2025-12-03 22:32:32 -0500

    feat: componente Experiencia con 4 cargos definidos

---
+++ src/components/experiencia.jsx
@@ -0,0 +1,33 @@
+function Experiencia() {
+  return (
+    <section>
+      <h3>Experiencia</h3>
+
+      <div>
+        <h4>Desarrollador Front-End</h4>
+        <p>Empresa imagiverse | 2022 - actualmente</p>
+        <p>Construcción de interfaces con React y consumo de APIs.</p>
+      </div>
+
+      <div>
+        <h4>Auxiliar TI</h4>
+        <p>Empresa modulaser| 2020 - 2022</p>
+        <p>Soporte técnico y mantenimiento de equipos.</p>
+      </div>
+
+      <div>
+        <h4>Proyecto Personal</h4>
+        <p>2019 - 2020</p>
+        <p>Desarrollo de una aplicación CRUD con React y Firebase.</p>
+      </div>
+
+      <div>
+        <h4>Proyecto Académico</h4>
+        <p>2018</p>
+        <p>Implementación de landing page con HTML, CSS y JS.</p>
+      </div>
+    </section>
+  );
+}
+
+export default Experiencia;
