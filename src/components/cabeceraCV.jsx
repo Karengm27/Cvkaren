25bfaf8daba45197755d31e35cab4ecbdf0db39e
karengm27 <karengmg27@gmail.com>
2025-12-03 22:29:15 -0500

    feat: componente CabeceraCV con datos personales actualizados

---
+++ src/components/cabeceraCV.jsx
@@ -0,0 +1,11 @@
+function CabeceraCV() {
+  return (
+    <header>
+      <h1>Karen Gonzalez Morales</h1>
+      <h2>Desarrollador Front-End Jr.</h2>
+      <p>📞 3014338069 | ✉ Karengmg27@gmail.com| Copacabana, Colombia</p>
+    </header>
+  );
+}
+
+export default CabeceraCV;
